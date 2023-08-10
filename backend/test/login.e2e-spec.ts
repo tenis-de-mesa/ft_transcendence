9deb7a9dab0e2c26a1c9efbd1b9d6668cc8d724c
Erick Medeiros <erickmedeirosbr@gmail.com>
2023-08-10 17:47:44 -0300

    test: improve and add more tests e2e of login
    (cherry picked from commit dfe2a06d5fbc7091413441d55b3ab8b121d21aa1)

--- backend/test/login.e2e-spec.ts
+++ backend/test/login.e2e-spec.ts
@@ -1,14 +1,24 @@
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, UnauthorizedException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
-import { AuthModule } from './../src/auth/auth.module';
+import { AuthModule } from '../src/auth/auth.module';
 import * as request from 'supertest';
-import { UserModule } from './../src/users/user.module';
+import { UserModule } from '../src/users/user.module';
 import { JwtModule } from '@nestjs/jwt';
-import { IntraModule } from './../src/intra/intra.module';
+import { IntraModule } from '../src/intra/intra.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { IntraService } from '../src/intra/intra.service';
+import { User } from '../src/users/user.entity';
 
 describe('OAuth in Intranet', () => {
   let app: INestApplication;
+
+  const FakeIntraService = {
+    getUserToken: async (code: string) => {
+      if (code == 'valid_code') return 'valid_token';
+      else throw new UnauthorizedException();
+    },
+    getUserInfo: async () => ({ id: 1, login: 'test' }),
+  };
 
   beforeAll(async () => {
     process.env = {
@@ -24,12 +34,16 @@ describe('OAuth in Intranet', () => {
         TypeOrmModule.forRoot({
           type: 'sqlite',
           database: ':memory:',
+          entities: [User],
           dropSchema: true,
           synchronize: true,
           logging: false,
         }),
       ],
-    }).compile();
+    })
+      .overrideProvider(IntraService)
+      .useValue(FakeIntraService)
+      .compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
@@ -39,7 +53,19 @@ describe('OAuth in Intranet', () => {
     await app.close();
   });
 
-  it('get', async () => {
+  it('does not allow login without code', async () => {
     return request(app.getHttpServer()).get('/auth/login').expect(400);
+  });
+
+  it('does not allow login with invalid code', async () => {
+    return request(app.getHttpServer())
+      .get('/auth/login?code=invalid_code')
+      .expect(401);
+  });
+
+  it('does allow login with valid code', async () => {
+    return request(app.getHttpServer())
+      .get('/auth/login?code=valid_code')
+      .expect(200);
   });
 });
