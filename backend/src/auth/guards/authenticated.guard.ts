8d1ef8002a7e960f8401cbfa8c0ec4dd1b03bdc7
Matheus Oliveira <31555284+araggohnxd@users.noreply.github.com>
2023-08-11 18:46:16 -0300

    fix: `AuthenticatedGuard` now throws Unauthorized instead of Forbidden

--- backend/src/auth/guards/authenticated.guard.ts
+++ backend/src/auth/guards/authenticated.guard.ts
@@ -1,10 +1,18 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 
 @Injectable()
 export class AuthenticatedGuard implements CanActivate {
-  async canActivate(context: ExecutionContext): Promise<boolean> {
+  canActivate(context: ExecutionContext): boolean {
     console.log('AuthenticatedGuard::canActivate()'); // TODO: Remove log line
     const request = context.switchToHttp().getRequest();
-    return request.isAuthenticated();
+    if (!request.isAuthenticated()) {
+      throw new UnauthorizedException();
+    }
+    return true;
   }
 }
