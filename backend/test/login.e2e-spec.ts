c9915bd6e55d0dab78772b49e12e160af71e5531
Erick Medeiros <erickmedeirosbr@gmail.com>
2023-08-23 15:13:32 -0300

    test: check status code 403 in login e2e

--- backend/test/login.e2e-spec.ts
+++ backend/test/login.e2e-spec.ts
@@ -1,43 +1,71 @@
 import { CanActivate, INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
-import { AuthModule } from '../src/auth/auth.module';
 import * as request from 'supertest';
-import { UsersModule } from '../src/users/users.module';
 import { AuthenticatedGuard, IntraAuthGuard } from '../src/auth/guards';
-import { TypeOrmConfigModule } from '../src/config/typeorm-config.module';
+import { AppModule } from '../src/app.module';
 
 describe('OAuth in Intranet', () => {
-  const mock_Guard: CanActivate = { canActivate: jest.fn(() => true) };
+  describe('not logged', () => {
+    const mock_Guard: CanActivate = { canActivate: jest.fn(() => false) };
 
-  let app: INestApplication;
+    let app: INestApplication;
 
-  beforeAll(async () => {
-    process.env = {
-      NODE_ENV: 'test',
-    };
+    beforeAll(async () => {
+      process.env = {
+        NODE_ENV: 'test',
+      };
 
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AuthModule, UsersModule, TypeOrmConfigModule],
-    })
-      .overrideGuard(AuthenticatedGuard)
-      .useValue(mock_Guard)
-      .overrideGuard(IntraAuthGuard)
-      .useValue(mock_Guard)
-      .compile();
+      const moduleFixture: TestingModule = await Test.createTestingModule({
+        imports: [AppModule],
+      })
+        .overrideGuard(AuthenticatedGuard)
+        .useValue(mock_Guard)
+        .overrideGuard(IntraAuthGuard)
+        .useValue(mock_Guard)
+        .compile();
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
+      app = moduleFixture.createNestApplication();
+      await app.init();
+    });
+
+    afterAll(async () => {
+      await app.close();
+    });
+
+    it('check status', async () => {
+      return request(app.getHttpServer()).get('/users/me').expect(403);
+    });
   });
 
-  afterAll(async () => {
-    await app.close();
-  });
+  describe('logged', () => {
+    const mock_Guard: CanActivate = { canActivate: jest.fn(() => true) };
 
-  it('user not logged in', async () => {
-    // return request(app.getHttpServer()).get('/users/me').expect(401);
-  });
+    let app: INestApplication;
 
-  it('user logged in', async () => {
-    return request(app.getHttpServer()).get('/users/me').expect(200);
+    beforeAll(async () => {
+      process.env = {
+        NODE_ENV: 'test',
+      };
+
+      const moduleFixture: TestingModule = await Test.createTestingModule({
+        imports: [AppModule],
+      })
+        .overrideGuard(AuthenticatedGuard)
+        .useValue(mock_Guard)
+        .overrideGuard(IntraAuthGuard)
+        .useValue(mock_Guard)
+        .compile();
+
+      app = moduleFixture.createNestApplication();
+      await app.init();
+    });
+
+    afterAll(async () => {
+      await app.close();
+    });
+
+    it('check status', async () => {
+      return request(app.getHttpServer()).get('/users/me').expect(200);
+    });
   });
 });
