5bf5042b39bcf8bebba2100ccdc1e2f4ee79b849
Lucas <lucasalexkuhn@gmail.com>
2023-09-01 11:30:12 -0300

    refactor gateway

--- backend/src/status/status.gateway.ts
+++ backend/src/status/status.gateway.ts
@@ -4,11 +4,11 @@ import {
   OnGatewayDisconnect,
   WebSocketServer,
 } from '@nestjs/websockets';
-import * as cookie from 'cookie';
 import { Server, Socket } from 'socket.io';
 import { SessionsService } from '../sessions/sessions.service';
 import { UsersService } from '../users/users.service';
 import { Session } from '../core/entities';
+import * as cookie from 'cookie';
 
 @WebSocketGateway({
   cors: {
@@ -18,56 +18,61 @@ import { Session } from '../core/entities';
   cookie: true,
 })
 export class StatusGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer()
+  server: Server;
+
   constructor(
     private readonly userService: UsersService,
     private readonly sessionService: SessionsService,
   ) {}
 
-  @WebSocketServer()
-  server: Server;
-
+  // Called when a client connects to the server
   async handleConnection(client: Socket) {
     const session = await this.getSession(client);
-    const socketId = client.id;
+    if (!session) return;
 
-    if (session) {
-      // Link the user session with the current socket id
-      await this.sessionService.updateSession(session.id, {
-        socketId: socketId,
-      });
-      // Update the user status to online
-      await this.userService.updateUser(session.user_id, { status: 'online' });
-      this.server.emit('userStatus', { id: session.user_id, status: 'online' });
-    }
+    this.updateSession(session, client.id);
+    this.updateUser(session.user_id, 'online');
+    this.emitUserStatus(session.user_id, 'online');
   }
 
+  // Called when a client disconnects from the server
   async handleDisconnect(client: Socket) {
     const session = await this.sessionService.getSessionBySocketId(client.id);
+    if (!session) return;
 
-    if (session) {
-      // Unlink the user session with the current socket id
-      await this.sessionService.updateSession(session.id, { socketId: null });
-      // Update the user status to offline
-      await this.userService.updateUser(session.user_id, { status: 'offline' });
-      this.server.emit('userStatus', {
-        id: session.user_id,
-        status: 'offline',
-      });
-    }
+    this.updateSession(session, null);
+    this.updateUser(session.user_id, 'offline');
+    this.emitUserStatus(session.user_id, 'offline');
   }
 
   async getSession(client: Socket): Promise<Session> {
     const sessionId = this.getSessionId(client);
-    const session = await this.sessionService.getSessionById(sessionId);
-    return session;
+    return await this.sessionService.getSessionById(sessionId);
+  }
+
+  updateSession(session: Session, socketId: string | null) {
+    this.sessionService.updateSession(session.id, { socketId });
+  }
+
+  updateUser(user_id: number, status: string) {
+    this.userService.updateUser(user_id, { status });
+  }
+
+  // Emit user status to all clients connected via websocket
+  emitUserStatus(user_id: number, status: string) {
+    this.server.emit('userStatus', { id: user_id, status });
   }
 
   getSessionId(client: Socket): string {
     const cookies = cookie.parse(client.handshake.headers.cookie || '');
-    const sessionId = cookies['connect.sid'];
-    if (!sessionId) {
-      return '';
-    }
+    const sessionCookie = cookies['connect.sid'];
+    return this.extractSessionId(sessionCookie);
+  }
+
+  extractSessionId(sessionId: string): string {
+    if (!sessionId) return '';
+
     const prefixIndex = sessionId.indexOf(':');
     const dotIndex = sessionId.indexOf('.');
     return sessionId.substring(prefixIndex + 1, dotIndex);
