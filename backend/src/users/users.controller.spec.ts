38d360ff0b9f52b2419f510cab388c79be4a9499
Erick Medeiros <erickmedeirosbr@gmail.com>
2023-09-21 12:14:19 -0300

    test: add unit test for UsersController->index

---
+++ backend/src/users/users.controller.spec.ts
@@ -0,0 +1,83 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UsersService } from './users.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { SessionEntity, UserEntity } from '../core/entities';
+import { UsersController } from './users.controller';
+import { Repository } from 'typeorm';
+
+const usersEntityList: UserEntity[] = [
+  new UserEntity({
+    id: 1,
+    login: 'login-1',
+    nickname: 'login-1',
+  } as UserEntity),
+  new UserEntity({
+    id: 2,
+    login: 'login-2',
+    nickname: 'login-2',
+  } as UserEntity),
+  new UserEntity({
+    id: 3,
+    login: 'login-3',
+    nickname: 'login-3',
+  } as UserEntity),
+];
+
+describe('UsersController', () => {
+  let app: TestingModule;
+  let usersController: UsersController;
+  let userRepository: Repository<UserEntity>;
+
+  beforeEach(async () => {
+    app = await Test.createTestingModule({
+      controllers: [UsersController],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(UserEntity),
+          useValue: {
+            find: jest.fn(),
+            save: jest.fn(),
+            findOneBy: jest.fn(),
+            update: jest.fn(),
+            findOne: jest.fn(),
+          },
+        },
+        {
+          provide: getRepositoryToken(SessionEntity),
+          useValue: {
+            createQueryBuilder: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    usersController = app.get<UsersController>(UsersController);
+    userRepository = app.get<Repository<UserEntity>>(
+      getRepositoryToken(UserEntity),
+    );
+  });
+
+  afterEach(async () => {
+    await app.close();
+  });
+
+  it('should be defined', async () => {
+    expect(app).toBeDefined();
+    expect(usersController).toBeDefined();
+    expect(userRepository).toBeDefined();
+  });
+
+  describe('index', () => {
+    it('show result', async () => {
+      // Arrange
+      jest.spyOn(userRepository, 'find').mockResolvedValueOnce(usersEntityList);
+
+      // Act
+      const result = await usersController.index();
+
+      // Assert
+      expect(result).toEqual(usersEntityList);
+    });
+  });
+});
