f13b743d71cd6a83f1a085b47bd9fc13014cdfc1
Lucas <lucasalexkuhn@gmail.com>
2023-09-24 21:36:57 -0300

    flip card using grid

--- frontend/src/components/UserForm.tsx
+++ frontend/src/components/UserForm.tsx
@@ -12,7 +12,7 @@ export default function UserForm({ user }: UserFormProps) {
   const [nickname, setNickname] = useState(user.nickname);
   const navigate = useNavigate();
   const handleUpdateAvatar = async (
-    event: React.ChangeEvent<HTMLInputElement>,
+    event: React.ChangeEvent<HTMLInputElement>
   ) => {
     const file = event.target.files?.[0];
     if (!file) {
