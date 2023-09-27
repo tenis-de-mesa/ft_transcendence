b510bd91aa0ec7b949bad5260bd216cfc324e55a
Erick Medeiros <erickmedeirosbr@gmail.com>
2023-09-27 19:01:05 -0300

    fix: import

--- backend/src/users/sessions/sessions.service.ts
+++ backend/src/users/sessions/sessions.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository, UpdateResult } from 'typeorm';
 import { UpdateSessionDto } from './dto';
-import { SessionEntity } from '../core/entities';
+import { SessionEntity } from '../../core/entities';
 
 @Injectable()
 export class SessionsService {
