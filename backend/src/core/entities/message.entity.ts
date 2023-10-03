2fea9e0a7fbb4c8052d22b923fb892177b9f1440
Erick Medeiros <erickmedeirosbr@gmail.com>
2023-10-03 10:26:58 -0300

    refactor: add optional constructors in entities
    chat entity and message entity

--- backend/src/core/entities/message.entity.ts
+++ backend/src/core/entities/message.entity.ts
@@ -11,4 +11,10 @@ export class Message {
 
   @ManyToOne(() => Chat, (chat) => chat.messages)
   chat: Chat;
+
+  constructor(message?: Message) {
+    this.id = message?.id;
+    this.content = message?.content;
+    this.chat = message?.chat;
+  }
 }
