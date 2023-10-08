41c5982b50eeb4f5e7fbd56d26814ddb8d84a673
Erick Medeiros <erickmedeirosbr@gmail.com>
2023-10-08 18:16:04 -0300

    feat: implement card profile for users in chat

--- frontend/src/routes/Chat.tsx
+++ frontend/src/routes/Chat.tsx
@@ -1,10 +1,19 @@
 import { socket } from "../socket";
 import { Form, Link, useLoaderData } from "react-router-dom";
-import { Chat, Message } from "../types/types";
+import { Chat, Message, User } from "../types/types";
 import { useEffect, useState } from "react";
 import Avatar from "../components/Avatar";
+import { Card } from "../components/Card";
+import { Typography } from "../components/Typography";
+import { Button } from "../components/Button";
+import { AiFillCloseCircle } from "react-icons/ai";
 
 export default function Chat() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [user, setUser] = useState<User>(null);
+
+  let lastUser: User | null = null;
+
   const [chat, setChat] = useState(useLoaderData() as Chat);
   const [newMessage, setNewMessage] = useState("");
   const chatId = chat.id;
@@ -32,21 +41,68 @@ export default function Chat() {
         <hr />
         <p>Messages:</p>
       </center>
+      {isOpen && (
+        <div className="dark fixed w-1/2">
+          <Card>
+            <Card.Title>
+              <div className="flex items-center justify-between">
+                <Avatar login={user?.login} avatarUrl={user?.avatarUrl} />
+                <Typography variant="h6">
+                  <Link to={`/profile/${user?.id}`}>{user?.nickname}</Link>
+                </Typography>
+                <Button
+                  IconOnly={<AiFillCloseCircle />}
+                  size="md"
+                  variant="info"
+                  onClick={() => setIsOpen(false)}
+                />
+              </div>
+            </Card.Title>
+            <Card.Body>
+              <div className="flip-card">
+                <div className="wrapper">
+                  <div className="card front">
+                    <p>
+                      <strong>Nickname:</strong> {user?.nickname}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </Card.Body>
+          </Card>
+        </div>
+      )}
       <ul>
-        {chat.messages.map((message) => (
-          <div key={message.id}>
-            <div>
-              <Avatar
-                login={message.user?.login}
-                avatarUrl={message.user?.avatarUrl}
-              />
-              <Link to={`/profile/${message.user?.id}`}>
-                {message.user?.nickname}:
-              </Link>
+        {chat.messages.map((message) => {
+          const showAvatar = lastUser?.id != message.user?.id;
+
+          lastUser = message.user;
+
+          return (
+            <div key={message.id}>
+              {showAvatar && (
+                <div className="flex gap-4">
+                  <Avatar
+                    login={message.user?.login}
+                    avatarUrl={message.user?.avatarUrl}
+                  />
+                  <button
+                    onClick={() => {
+                      setIsOpen(!isOpen);
+                      setUser(message.user);
+                    }}
+                  >
+                    <Typography variant="h6">
+                      {message.user?.nickname}:
+                    </Typography>
+                  </button>
+                </div>
+              )}
+
+              <div className="pl-16">{message.content}</div>
             </div>
-            {" "} {message.content}
-          </div>
-        ))}
+          );
+        })}
       </ul>
       <Form
         className="messageInput"
@@ -57,6 +113,7 @@ export default function Chat() {
           type="text"
           name="message"
           value={newMessage}
+          className="bg-gray-500"
           onChange={(e) => setNewMessage(e.target.value)}
         />
         <button type="submit" hidden>
