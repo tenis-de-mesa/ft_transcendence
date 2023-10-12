3ee57f21c40710f25390956f7ce7bb6c2562d66f
Fabio Machado <git.ichmi@gmail.com>
2023-10-12 13:41:28 -0300

    style: newstyles property

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
-import "./styles/index.css";
 
 import "./styles/index.css";
 
