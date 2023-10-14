1697744d5fadd53aeee4a0bba7d8fb4973a0bea4
Fabio Machado <git.ichmi@gmail.com>
2023-10-14 13:33:27 -0300

    update: preview routes ui

--- frontend/src/routes/ChatNew.tsx
+++ frontend/src/routes/ChatNew.tsx
@@ -1,23 +1,38 @@
 import { Form, useLoaderData } from "react-router-dom";
 import { User } from "../types/types";
+import { Card } from "../components/Card";
+import { Typography } from "../components/Typography";
+import { Input } from "../components/Input";
+import { useState } from "react";
 
 export default function ChatNew() {
+  const [message, setMessage] = useState("");
+
   const chatUser = useLoaderData() as User;
 
   return (
-    <div className="card">
-      <h2>
-        <center>
+    <Card className="h-full">
+      <Card.Title>
+        <Typography variant="h6">
           New chat with <strong>{chatUser.nickname}</strong>
-        </center>
-      </h2>
-      <Form method="POST" action="/chats" className="messageInput">
-        <input type="hidden" name="users[]" value={chatUser.id} />
-        <input type="text" name="message" className="bg-gray-500" />
-        <button type="submit" hidden>
-          Create Chat
-        </button>
-      </Form>
-    </div>
+        </Typography>
+      </Card.Title>
+      <Card.Body position="left" className="flex flex-col justify-between h-[calc(100%-4rem)]">
+        <>
+          <div></div>
+          <div>
+            <Form method="POST" action="/chats">
+              <input type="hidden" name="users[]" value={chatUser.id} />
+              <Input type="text" name="message" value={message} onChange={(e) => setMessage(e.target.value)} placeholder="Enter your message" />
+              <button type="submit" hidden>
+                <Typography variant="sm">
+                  Create Chat
+                </Typography>
+              </button>
+            </Form>
+          </div>
+        </>
+      </Card.Body>
+    </Card>
   );
 }
--- frontend/src/routes/Leaderboard.tsx
+++ frontend/src/routes/Leaderboard.tsx
@@ -1,8 +1,57 @@
-import { DataTable } from "../components/DataTable";
+import { useMemo, useState } from "react";
+
+import {
+  ColumnDef,
+  createColumnHelper
+} from '@tanstack/react-table'
+
 import { Typography } from "../components/Typography";
-import { tableData } from "../data";
+import { Data, tableData } from "../data";
+
+import Table from "../components/Table";
+
+const columnHelper = createColumnHelper<Data>();
 
 const Leaderboard = () => {
+  const columns = useMemo<ColumnDef<Data>[]>(() => [
+    columnHelper.accessor("id", {
+      cell: info => <span className="font-bold dark:text-gray-50">{info.getValue()}</span>
+    }),
+    columnHelper.accessor("nickname", {
+      header: () => <span>Nickname</span>,
+      cell: info => <i>{info.getValue()}</i>,
+    }),
+    columnHelper.accessor("email", {
+      header: () => "Email",
+      cell: info => info.renderValue(),
+    }),
+    columnHelper.accessor("total_games", {
+      header: () => <span>Total games</span>,
+    }),
+    columnHelper.accessor("wins", {
+      header: "Wins",
+    }),
+    columnHelper.accessor("online", {
+      header: "Status",
+      cell: info => {
+        const isOnline = info.getValue();
+        return (isOnline ? 
+          <div className="flex items-center">
+            <div className="h-2.5 w-2.5 rounded-full bg-success-500 mr-2"></div>
+            <span className='text-success-500'>Online</span> 
+          </div>
+          :
+          <div className="flex items-center">
+            <div className="h-2.5 w-2.5 rounded-full bg-error-500 mr-2"></div>
+            <span className='text-error-500'>Offline</span> 
+          </div>
+        )
+      },
+    }),
+  ], []);
+
+  const [data] = useState(() => [...tableData])
+
   return (
     <>
       <Typography variant="h5">
@@ -10,7 +59,7 @@ const Leaderboard = () => {
       </Typography>
 
       <div className="h-[92%]">
-        <DataTable dataset={tableData} />
+        <Table columns={columns} data={data} />
       </div>
     </>
   )
