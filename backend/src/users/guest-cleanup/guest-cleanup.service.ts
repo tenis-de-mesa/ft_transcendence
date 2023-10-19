8d90f49e4590ae79d222f82a92411b812a5d6a30
Lucas <lucasalexkuhn@gmail.com>
2023-10-19 16:46:29 -0300

    emit to online users that a guest user is offline if it was deleted

--- backend/src/users/guest-cleanup/guest-cleanup.service.ts
+++ backend/src/users/guest-cleanup/guest-cleanup.service.ts
@@ -1,16 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import { UsersService } from '../users.service';
+import { StatusGateway } from '../status/status.gateway';
+import { UserStatus } from '../../core/entities';
 
 @Injectable()
 export class GuestCleanupService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly statusGateway: StatusGateway,
+  ) {}
 
   async cleanupGuestUsers(): Promise<void> {
     const guestUsers = await this.usersService.findObsoleteGuestUsers();
 
     for (const user of guestUsers) {
       await this.usersService.deleteUser(user.id);
+      this.statusGateway.emitUserStatus(user.id, UserStatus.OFFLINE);
     }
   }
 
