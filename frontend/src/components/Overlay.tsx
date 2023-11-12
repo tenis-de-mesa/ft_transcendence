d7fab37968009ca05c63e3fd623b427323a5835c
Matheus Oliveira <31555284+araggohnxd@users.noreply.github.com>
2023-11-12 00:57:18 -0300

    feat: add blur to `Overlay` component

--- frontend/src/components/Overlay.tsx
+++ frontend/src/components/Overlay.tsx
@@ -1,3 +1,5 @@
 export const Overlay = () => {
-  return <div className="fixed inset-0 z-[1000] bg-gray-900/50"></div>;
+  return (
+    <div className="fixed inset-0 z-[1000] backdrop-blur-sm bg-gray-900/50"></div>
+  );
 };
