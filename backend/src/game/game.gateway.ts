bd7f1af47ca27c1de504271e165adf81abff94b6
Fabio Machado <git.ichmi@gmail.com>
2023-11-13 00:27:59 -0300

    feat(backend): initial game mechanics

--- backend/src/game/game.gateway.ts
+++ backend/src/game/game.gateway.ts
@@ -16,17 +16,32 @@ import { Position2D } from './interfaces';
 
 type Player = {
   id: string;
-  x: number;
   y: number;
+  playerType: string;
 };
 
-type Move = {
+interface Ball {
+  x: number;
+  y: number;
+  speedX: number;
+  speedY: number;
+  radius: number;
+}
+
+interface GameRoom {
   id: number;
-  move: {
-    x: number;
-    y: number;
-  };
+  players: Player[];
+}
+
+type Direction = {
+  up: boolean;
+  down: boolean;
 };
+
+interface Score {
+  playerOne: number;
+  playerTwo: number;
+}
 
 @WebSocketGateway({
   namespace: 'games',
@@ -41,101 +56,200 @@ export class GameGateway
 {
   @WebSocketServer()
   server: Server;
-  MAXWIDTH = 700;
-  MAXHEIGHT = 600;
-  players: object;
-  total_players: number;
-  ball_x: number;
-  ball_y: number;
+
+  gameRooms: GameRoom[];
+  serverTotalRooms: number;
+
+  players: Record<string, Player | undefined>;
+  serverTotalPlayers: number;
+
+  windowWidth = 700;
+  windowHeight = 600;
+
+  ball: Ball;
+  score: Score;
 
   vecDirX = 1;
   vecDirY = 1;
 
   interval;
 
-  constructor(
-    private readonly sessionService: SessionsService,
-    private readonly gameService: GameService,
-  ) {}
+  constructor(private readonly sessionService: SessionsService) {
+    this.interval = setInterval(() => {
+      this.gameLoop();
+    }, 16);
+  }
 
   afterInit(server: Server) {
-    server;
-    this.total_players = 0;
+    this.gameRooms = [];
+    this.serverTotalRooms = 0;
+
     this.players = {};
+    this.serverTotalPlayers = 0;
+
+    this.ball = {
+      x: this.windowWidth / 2,
+      y: this.windowHeight / 2,
+      speedX: 4,
+      speedY: 4,
+      radius: 16,
+    };
+
+    this.score = {
+      playerOne: 0,
+      playerTwo: 0,
+    };
   }
 
-  handleConnection(client: Socket) {
-    console.log('Game connection:', client.id);
-    const player: Player = { id: client.id, x: 0, y: 0 };
-    this.players[client.id] = player;
-    this.ball_x = this.MAXWIDTH / 2;
-    this.ball_y = this.MAXHEIGHT / 2;
+  handleConnection(clientSocket: Socket) {
+    console.log('New client connection:', clientSocket.id);
 
-    this.total_players++;
-    this.server.emit('ON_PLAYERS_UPDATE', this.players);
+    let playerType: string;
+    if (this.serverTotalPlayers % 2 === 0) {
+      playerType = 'left';
+    } else {
+      playerType = 'right';
+    }
 
-    this.interval = setInterval(() => {
-      this.ball_x = this.ball_x + this.vecDirX;
-      this.ball_y = this.ball_y + this.vecDirY;
-      this.onBallUpdate();
-    }, 100);
+    const newPlayerConnection: Player = {
+      id: clientSocket.id,
+      y: 0,
+      playerType: playerType,
+    };
+
+    const newPlayerStringId: string = clientSocket.id;
+
+    this.players[newPlayerStringId] = newPlayerConnection;
+    this.serverTotalPlayers++;
+
+    this.addPlayerToRoom(newPlayerConnection);
+
+    this.server.emit('socketConnection', this.players);
+    clientSocket.emit('playerConnection', newPlayerConnection.id);
+
+    // this.server.emit('ballPosition', this.ball);
   }
 
-  handleDisconnect(client: Socket) {
-    console.log('Game disconnect:', client.id);
-    this.players[client.id] = undefined;
-    this.total_players--;
-    this.server.emit('ON_PLAYERS_UPDATE', this.players);
+  handleDisconnect(clientSocket: Socket) {
+    console.log('Client disconnected:', clientSocket.id);
+
+    const playerStringId: string = clientSocket.id;
+    this.players[playerStringId] = undefined;
+    this.serverTotalPlayers--;
+
+    this.removePlayerFromRoom(playerStringId);
+
+    this.server.emit('socketConnection', this.players);
+    clientSocket.emit('playerDisconnection', playerStringId);
   }
 
-  @SubscribeMessage('ON_PLAYER_MOVE')
-  async handleEventMove(
-    @ConnectedSocket() client: Socket,
-    @MessageBody() movement: Move,
+  addPlayerToRoom(player: Player) {
+    const availableRoom = this.gameRooms.find(
+      (room) => room.players.length < 2,
+    );
+
+    if (availableRoom) {
+      availableRoom.players.push(player);
+    } else {
+      const newRoom: GameRoom = {
+        id: this.gameRooms.length + 1,
+        players: [player],
+      };
+
+      this.gameRooms.push(newRoom);
+      this.serverTotalRooms++;
+    }
+  }
+
+  removePlayerFromRoom(playerId: string) {
+    for (const room of this.gameRooms) {
+      const playerIndex = room.players.findIndex(
+        (player) => player.id === playerId,
+      );
+
+      if (playerIndex !== -1) {
+        room.players.splice(playerIndex, 1);
+
+        if (room.players.length === 0) {
+          const roomIndex = this.gameRooms.findIndex((r) => r.id === room.id);
+          this.gameRooms.splice(roomIndex, 1);
+        }
+
+        break;
+      }
+    }
+  }
+
+  gameLoop() {
+    this.ball.x += this.ball.speedX;
+    this.ball.y += this.ball.speedY;
+
+    if (this.ball.x <= 0 || this.ball.x >= this.windowWidth) {
+      this.ball.speedX = -this.ball.speedX;
+    }
+
+    if (this.ball.y <= 0 || this.ball.y >= this.windowHeight) {
+      this.ball.speedY = -this.ball.speedY;
+    }
+
+    for (const playerId in this.players) {
+      const player = this.players[playerId];
+
+      if (player) {
+        let playerX: number;
+        if (player.playerType === 'left') {
+          playerX = 10;
+        } else if (player.playerType === 'right') {
+          playerX = this.windowWidth - 20 - 10;
+        }
+        const playerY = player.y;
+
+        const playerWidth = 10;
+        const playerHeight = 100;
+
+        const ballInXRange =
+          this.ball.x + this.ball.radius > playerX &&
+          this.ball.x - this.ball.radius < playerX + playerWidth;
+
+        const ballInYRange =
+          this.ball.y + this.ball.radius > playerY &&
+          this.ball.y - this.ball.radius < playerY + playerHeight;
+
+        if (ballInXRange && ballInYRange) {
+          // randomize the colission response?
+          this.ball.speedX = -this.ball.speedX;
+        }
+      }
+    }
+
+    this.server.emit('updateBallPosition', { x: this.ball.x, y: this.ball.y });
+  }
+
+  @SubscribeMessage('movePlayer')
+  async handlePlayerMovement(
+    @ConnectedSocket() clientSocket: Socket,
+    @MessageBody() direction: Direction,
   ) {
-    const player = this.players[client.id];
+    const playerId = clientSocket.id;
 
-    player.x = player.x + (movement.move.x || 0);
-    player.y = player.y + (movement.move.y || 0);
-    if (player.y > this.MAXHEIGHT) {
-      player.y = this.MAXHEIGHT;
+    if (direction.up) {
+      if (this.players[playerId].y < 10) {
+        this.players[playerId].y = 0;
+      } else {
+        this.players[playerId].y -= 10;
+      }
+    }
+    if (direction.down) {
+      if (this.players[playerId].y > this.windowHeight - 100 - 10) {
+        this.players[playerId].y = this.windowHeight - 100;
+      } else {
+        this.players[playerId].y += 10;
+      }
     }
 
-    this.server.emit('ON_PLAYERS_UPDATE', this.players);
-  }
-
-  @SubscribeMessage('ON_BALL_MOVE')
-  async handleEventBallMove(
-    @ConnectedSocket() client: Socket,
-    @MessageBody() movement: Move,
-  ) {
-    const move: Position2D = { x: this.MAXWIDTH / 2, y: this.MAXHEIGHT / 2 };
-    this.server.emit('ON_BALL_UPDATE', move);
-  }
-
-  @SubscribeMessage('ping')
-  async pingPong(@ConnectedSocket() client: Socket) {
-    client.emit('pong');
-  }
-
-  async onBallUpdate() {
-    if (this.ball_x > this.MAXWIDTH) {
-      this.ball_x = this.MAXWIDTH;
-      this.vecDirX = -this.vecDirX;
-    }
-    if (this.ball_x < 0) {
-      this.ball_x = 0;
-      this.vecDirX = -this.vecDirX;
-    }
-    if (this.ball_y > this.MAXHEIGHT) {
-      this.ball_y = this.MAXHEIGHT;
-      this.vecDirY = -this.vecDirY;
-    }
-    if (this.ball_y < 0) {
-      this.ball_y = 0;
-      this.vecDirY = -this.vecDirY;
-    }
-    const move: Position2D = { x: this.ball_x, y: this.ball_y };
-    this.server.emit('ON_BALL_UPDATE', move);
+    this.server.emit('updatePlayerPosition', {
+      playerId,
+      position: this.players[playerId],
+    });
   }
 }
