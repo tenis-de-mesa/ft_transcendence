d03f3bc932d7b84e43da5f8b3575e9710f2dd009
Matheus Oliveira <31555284+araggohnxd@users.noreply.github.com>
2023-11-30 11:17:39 -0300

    [FIX] Properly process errors in loaders (#171)
    * fix: properly show error when accessing non existent/not member chats
    
    * fix: properly throw on loader error and capture in `ErrorBoundary`

--- frontend/src/loaders/loadUserById.ts
+++ frontend/src/loaders/loadUserById.ts
@@ -1,7 +1,25 @@
 import { LoaderFunctionArgs } from "react-router-dom";
+import { makeRequest } from "../api";
+import { User } from "../types";
 
 export async function loadUserById({ params }: LoaderFunctionArgs) {
-  return fetch(`http://localhost:3001/users/${params.id}`, {
-    credentials: "include",
+  const { data, error } = await makeRequest<User>(`/users/${params.id}`, {
+    method: "GET",
   });
+
+  if (error) {
+    if (error.statusCode === 401) {
+      throw new Response("Not Found", {
+        status: 404,
+        statusText: "Not Found",
+      });
+    }
+
+    throw new Response(error.message, {
+      status: error.statusCode,
+      statusText: error.message,
+    });
+  }
+
+  return data;
 }
