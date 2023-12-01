ba9c13809042cccf1102703793a7ec9ac300dc3c
Erick Medeiros <erickmedeirosbr@gmail.com>
2023-12-01 14:56:11 -0300

    refactor: gateway disconnection

--- backend/src/game/game.gateway.ts
+++ backend/src/game/game.gateway.ts
@@ -86,22 +86,30 @@ export class GameGateway
   handleDisconnect(clientSocket: Socket) {
     const user: UserEntity = clientSocket.handshake.auth?.user;
 
-    if (user) {
-      this.queues.matchVanilla = this.queues.matchVanilla.filter(
-        (u) => u.id != user.id,
-      );
-      this.queues.matchPowerUp = this.queues.matchPowerUp.filter(
-        (u) => u.id != user.id,
-      );
-      this.queues.invites = this.queues.invites.filter((u) => {
-        if (u.user.id == user.id) {
-          this.sendUpdateInviteList(u.guest.id);
-        } else if (u.guest.id == user.id) {
-          this.sendUpdateInviteList(u.user.id);
-        }
-        return u.user.id != user.id && u.guest.id != user.id;
-      });
+    if (!user) {
+      return;
     }
+
+    this.queues.matchVanilla = this.queues.matchVanilla.filter(
+      (u) => u.id != user.id,
+    );
+
+    this.queues.matchPowerUp = this.queues.matchPowerUp.filter(
+      (u) => u.id != user.id,
+    );
+
+    const listOfUsersForUpdateInvites: Set<number> = new Set();
+
+    this.queues.invites = this.queues.invites.filter((u) => {
+      if (u.user.id == user.id) {
+        listOfUsersForUpdateInvites.add(u.guest.id);
+      } else if (u.guest.id == user.id) {
+        listOfUsersForUpdateInvites.add(u.user.id);
+      }
+      return u.user.id != user.id && u.guest.id != user.id;
+    });
+
+    listOfUsersForUpdateInvites.forEach((id) => this.sendUpdateInviteList(id));
   }
 
   private async validate(client: Socket) {
