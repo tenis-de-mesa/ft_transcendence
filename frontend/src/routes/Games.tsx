79c5c24e60e69bacde469d14db84f5182270a41f
Lucas Kuhn <lucasalexkuhn@gmail.com>
2023-12-03 07:43:42 -0300

    Default message for empty state on channels and games (#193)

--- frontend/src/routes/Games.tsx
+++ frontend/src/routes/Games.tsx
@@ -3,6 +3,7 @@ import { ColumnDef, createColumnHelper } from "@tanstack/react-table";
 import { Link, useLoaderData } from "react-router-dom";
 import { Game } from "../types";
 import Table from "../components/Table";
+import { Button } from "../components";
 
 const Games = () => {
   const games = useLoaderData() as Game[];
@@ -49,12 +50,30 @@ const Games = () => {
   ];
 
   return (
-    <div>
-      <Typography variant="h5" className="mb-5">
-        Games
-      </Typography>
-      <Table columns={columns as ColumnDef<unknown>[]} data={games} />
-    </div>
+    <>
+      {games.length === 0 ? (
+        <div className="grid align-center content-center justify-center h-full">
+          <Typography variant="lg" className="text-center">
+            There are no games on the Leaderboard yet.
+          </Typography>
+          <Link to="/" className="text-center p-3">
+            <Button variant={"info"} size="lg" className="inline">
+              Go home to join a game
+            </Button>
+          </Link>
+        </div>
+      ) : (
+        <>
+          <Typography variant="h5" className="mb-6">
+            Leaderboard
+          </Typography>
+          <Table
+            columns={columns as unknown as ColumnDef<unknown>[]}
+            data={games}
+          />
+        </>
+      )}
+    </>
   );
 };
 
