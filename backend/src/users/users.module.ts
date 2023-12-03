509a3797af6650e21cbcb5d26cc1d99965ee2a38
Erick Medeiros <erickmedeirosbr@gmail.com>
2023-12-02 21:15:25 -0300

    refactor: remove unnucessary EventEmittterModule

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -7,27 +7,13 @@ import { S3ClientProvider } from '../lib/aws/s3Client';
 import { BlockListEntity } from '../core/entities/blockList.entity';
 import { SessionsService } from './sessions/sessions.service';
 import { UsersGateway } from './users.gateway';
-import { EventEmitter2, EventEmitterModule } from '@nestjs/event-emitter';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([UserEntity, SessionEntity, BlockListEntity]),
-    EventEmitterModule.forRoot(),
   ],
   controllers: [UsersController],
-  providers: [
-    UsersService,
-    SessionsService,
-    S3ClientProvider,
-    UsersGateway,
-    EventEmitter2,
-  ],
-  exports: [
-    UsersService,
-    SessionsService,
-    S3ClientProvider,
-    UsersGateway,
-    EventEmitter2,
-  ],
+  providers: [UsersService, SessionsService, S3ClientProvider, UsersGateway],
+  exports: [UsersService, SessionsService, S3ClientProvider, UsersGateway],
 })
 export class UsersModule {}
